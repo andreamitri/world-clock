1e17ea7dd1cfb2038b001ab28c9a6e574bb1585d
andreamitri <160364123+andreamitri@users.noreply.github.com>
2024-03-20 22:11:04 +0000

    added another city

--- javascript/index.js
+++ javascript/index.js
@@ -24,6 +24,16 @@ function updateTime() {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  // Rome
+  let romeElement = document.querySelector("#rome");
+  if (romeElement) {
+    let romeDateElement = romeElement.querySelector(".date");
+    let romeTimeElement = romeElement.querySelector(".time");
+    let romeTime = moment().tz("Europe/Rome");
+
+    romeDateElement.innerHTML = romeTime.format("MMMM	Do YYYY");
+    romeTimeElement.innerHTML = romeTime.format("h:mm:ss [<small>]A[</small>]");
+  }
 }
 
 function updateCity(event) {
